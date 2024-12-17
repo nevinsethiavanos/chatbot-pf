7e533e375df4fc00688d385ea0932768ed007536
nevinsethiavanos <nevin.sethi@avanos.com>
2024-12-17 09:47:44 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Avanos Helpdesk Chatbot"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
-    chat_description: str = "This is the avanos chatbot."
+    chat_description: str = "This is the avanos chatbot with correct photos and names."
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
